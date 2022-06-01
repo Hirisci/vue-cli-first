5a56f0cf33790637a8ea45d7d180ec872d192f16
Hirisci <www.alan92@icloud.com>
2022-06-01 09:47:34 +0200

    fuxato

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TitleBase />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TitleBase from "./components/TitleBase.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    TitleBase,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/TitleBase.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>title</h1>
+</template>
+
+<script>
+export default {
+  name: "TitleBase",
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
